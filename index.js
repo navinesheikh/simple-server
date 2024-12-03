002df1d4ac97e322ba1d2c41748ed040a397407b
NavineSheikh <navinesheikh@hotmail.com>
2024-12-03 11:24:34 +0000

    small change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World brev");
+    res.end("Hello World brev!");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 });
